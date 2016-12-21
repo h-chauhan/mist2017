b93c36a9e1ed674ea528d07ad7dbcba8ffca43ba
harshita97 <hrshita97@gmail.com>
2016-12-21 14:36:24 +0530

    levels

--- question/models.py
+++ question/models.py
@@ -2,12 +2,14 @@ from django.db import models
 
 # Create your models here.
 class Question(models.Model):
+    level = models.IntegerField()
     question_text = models.CharField(max_length=200)
     image = models.ImageField()
     html = models.TextField()
     script = models.TextField()
     video = models.FileField()
     audio = models.FileField()
+     
     def __str__(self):
         return self.question_text
 
