37797cc05349231bdfcf27e036a09d77d7709048
harshita97 <hrshita97@gmail.com>
2016-12-21 14:04:48 +0530

    models

--- question/models.py
+++ question/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    image = models.ImageField()
+    html = models.TextField()
+    script = models.TextField()
+    video = models.FileField()
+    audio = models.FileField()
+    def __str__(self):
+        return self.question_text
+
+class Answers(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    ans_text = models.CharField(max_length=200)
+    def __str__(self):
+        return self.ans_text
