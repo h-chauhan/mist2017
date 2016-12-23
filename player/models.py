90e6661fa799440d6dd8f9394b53a1e9c250b854
harshita97 <hrshita97@gmail.com>
2016-12-23 18:50:32 +0530

    player

--- player/models.py
+++ player/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Player(models.Model):
+    name = models.CharField(max_length=200) 
+    isAdmin = models.BinaryField()
+    fbID = models.CharField(max_length=200)
+    Email = models.EmailField(max_length=254)
+    UserToken = models.CharField(max_length=200)
+    level = models.IntegerField()
+    timeStamp = models.DateTimeField('date published')
+    def __str__(self):
+        return self.name
