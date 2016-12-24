46e53073d19d0b4e07f78da49a883686501a7a4e
Himanshu Chauhan <h.chauhan.1997@ieee.org>
2016-12-24 15:53:48 +0530

    Fb login JS setup

---
+++ login/urls.py
@@ -0,0 +1,6 @@
+from django.conf.urls import url
+from login import views
+
+urlpatterns = [
+    url(r'^$', views.login, name='login'),
+]
--- mist2017/settings.py
+++ mist2017/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'question.apps.QuestionConfig',
+    'player.apps.PlayerConfig',
+    'login.apps.LoginConfig',
 ]
 
 MIDDLEWARE = [
@@ -55,7 +57,7 @@ ROOT_URLCONF = 'mist2017.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates'),],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
