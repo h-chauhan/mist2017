96a3a9fa3a4903930a7fa06d8f852cae26c5eb20
harshita97 <hrshita97@gmail.com>
2017-01-02 14:57:25 +0530

    questionAppViewGetques

--- question/views.py
+++ question/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def get_question(request,lvl):
+    player = get_object_or_404(Player, user=request.user)
+    ques = get_object_or_404(Question, level=lvl)
+
+    if request.POST.get(level)<=lvl:
+        return render(request,"index.html",level)
+    else redirect(lvl,permanent=False)
