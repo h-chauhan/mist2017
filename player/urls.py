f77104a1aef1015782863ef227fd5da4a4489711
harshita97 <hrshita97@gmail.com>
2017-01-03 20:53:55 +0530

    playerAppView

--- player/urls.py
+++ player/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
 from player import views
 
+
 urlpatterns = [
     url(r'create/', views.createPlayer, name='create'),
 ]
--- player/views.py
+++ player/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, HttpResponse
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.decorators.csrf import csrf_exempt
 from player.models import Player
 from django.utils import timezone
@@ -17,3 +18,25 @@ def createPlayer(request):
                         levelTime=timezone.now(), startTime=timezone.now(), user=request.user)
         player.save()
     return HttpResponse("Success")
+
+def getPlayer(request):
+    player = get_object_or_404(Player, user=request.user)
+    s=serialzers.serialize('json', [player])
+    return HttpResponse(s,content_type="application/json")
+
+def playerList(request):
+    player_list = Player.objects.all()
+    paginator = Paginator(player_list, 25) # Show 25 player per page
+
+    page = request.GET.get('page')
+    try:
+        player = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer, deliver first page.
+        player = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range (e.g. 9999), deliver last page of results.
+        player = paginator.page(paginator.num_pages)
+
+    return render(request, 'list.html', {'players': player})
+
