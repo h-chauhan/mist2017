20e1f3ef9909f26341932a50b44b5bc417cf47bb
Chavqueen <chavqueen.cs@gmail.com>
2017-01-05 14:58:26 +0530

    submitAnswer view changed

--- question/views.py
+++ question/views.py
@@ -9,3 +9,18 @@ def get_question(request,lvl):
     if request.POST.get(level)<=lvl:
         return render(request,"index.html",level)
     else redirect(lvl,permanent=False)
+
+def submitAnswer(request,lvl):
+    ques = get_object_or_404(Question, level=lvl)
+    player = get_object_or_404(Player, user=request.user)
+    answers = Answer.objects.filter(question=ques)
+    status = False 
+
+    for answer in answers:
+        if answer.ans == lower(request.POST.get('answer')):
+            player.level += 1
+            player.save()
+            status = True
+            break
+
+    return HttpResponse(status)           
