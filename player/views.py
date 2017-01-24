025514c1e01ca09fc787f2611f974eb8215f9706
deepikakumari97 <deepika.dk97@gmail.com>
2017-01-24 15:49:45 +0530

    changes in playerlist

--- player/views.py
+++ player/views.py
@@ -3,6 +3,7 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.decorators.csrf import csrf_exempt
 from player.models import Player
 from django.utils import timezone
+from allauth.socialaccount.models import SocialAccount
 import json
 
 # Create your views here.
@@ -15,23 +16,32 @@ def createPlayer(request):
         player.save()
     return HttpResponseRedirect(reverse('question'))
 
-def getPlayer(request):
-    player = get_object_or_404(Player, user=request.user)
-    s=serialzers.serialize('json', [player])
-    return HttpResponse(s,content_type="application/json")
 
 def playerList(request):
+    player = get_object_or_404(Player, user=request.user)
     player_list = Player.objects.order_by('-level', 'levelTime', 'pk')
     paginator = Paginator(player_list, 2) # Show 25 player per page
 
     page = request.GET.get('page')
     try:
-        player = paginator.page(page)
+        player_list = paginator.page(page)
     except PageNotAnInteger:
         # If page is not an integer, deliver first page.
-        player = paginator.page(1)
+        player_list = paginator.page(1)
     except EmptyPage:
         # If page is out of range (e.g. 9999), deliver last page of results.
-        player = paginator.page(paginator.num_pages)
+        player_list = paginator.page(paginator.num_pages)
+
+    leaderboard = []
+    for p in player_list:
+        leaderboard.append(p)
+
+    print(leaderboard)
+
+    context = {
+        'player': player,
+        'social_account': SocialAccount.objects.get(user=player.user),
+        'player_list': player_list,
+    }
 
     return render(request, 'player/list.html', {'players': player})
