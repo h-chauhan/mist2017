ce6190dc7b4a24400f6fd1e32bf6ee5eacd81ea3
Himanshu Chauhan <hchauhan1404@outlook.com>
2017-01-24 16:01:37 +0530

    leaderboard

--- player/views.py
+++ player/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, HttpResponseRedirect, HttpResponse, reverse
+from django.shortcuts import render, HttpResponseRedirect, HttpResponse, reverse, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth.decorators import login_required
 from player.models import Player
 from django.utils import timezone
 from allauth.socialaccount.models import SocialAccount
@@ -16,7 +17,7 @@ def createPlayer(request):
         player.save()
     return HttpResponseRedirect(reverse('question'))
 
-
+@login_required
 def playerList(request):
     player = get_object_or_404(Player, user=request.user)
     player_list = Player.objects.order_by('-level', 'levelTime', 'pk')
@@ -34,14 +35,17 @@ def playerList(request):
 
     leaderboard = []
     for p in player_list:
-        leaderboard.append(p)
-
-    print(leaderboard)
+        leaderboard.append({
+            'player': p,
+            'social_account': SocialAccount.objects.get(user=p.user),
+            'rank': p.rank()
+        })
 
     context = {
         'player': player,
         'social_account': SocialAccount.objects.get(user=player.user),
-        'player_list': player_list,
+        'leaderboard': leaderboard,
+        'rank': player.rank()
     }
 
-    return render(request, 'player/list.html', {'players': player})
+    return render(request, 'player/list.html', context)
