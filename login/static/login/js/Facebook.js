731036623557d0cf2f0a7b69c53e7ee948451ace
Himanshu Chauhan <hchauhan1404@outlook.com>
2017-01-25 15:10:19 +0530

    beautified

--- login/static/login/js/Facebook.js
+++ login/static/login/js/Facebook.js
@@ -1,52 +1,50 @@
-window.fbAsyncInit = function() {
-        FB.init({
-            appId: '1395586523786497',
-            xfbml: true,
-            version: 'v2.8'
-        });
-
- FB.getLoginStatus(function(response) {
-                if (response.status === 'connected') {
-                    $.post("/rest-auth/facebook/", {
-                        "access_token": response.authResponse.accessToken
-                    }, function(data, status) {       
-                        console.log("Data: " + data.key + "\nStatus: " + status);   
-                    });
-                    $("#play-btn").show();
-                    $("#login-btn").hide();
-                } else {
-                    $("#play-btn").hide();
-                    $("#login-btn").show();
-                }
+window.fbAsyncInit = function () {
+    FB.init({
+        appId: '1395586523786497',
+        xfbml: true,
+        version: 'v2.8'
+    });
+    FB.getLoginStatus(function (response) {
+        if (response.status === 'connected') {
+            $.post("/rest-auth/facebook/", {
+                "access_token": response.authResponse.accessToken
+            }, function (data, status) {       
+                console.log("Data: " + data.key + "\nStatus: " + status);   
             });
-
-FB.AppEvents.logPageView();
-    };
-    (function(d, s, id) {
-        var js, fjs = d.getElementsByTagName(s)[0];
-        if (d.getElementById(id)) {
-            return;
+            $("#play-btn").show();
+            $("#login-btn").hide();
+        } else {
+            $("#play-btn").hide();
+            $("#login-btn").show();
         }
-        js = d.createElement(s);
-        js.id = id;
-        js.src = "https://connect.facebook.net/en_US/sdk.js";
-        fjs.parentNode.insertBefore(js, fjs);
-    }(document, 'script', 'facebook-jssdk'));
-
-    function checkLoginState() {
-     FB.getLoginStatus(function(response) {
-                if (response.status === 'connected') {
-                    $.post("/rest-auth/facebook/", {
-                        "access_token": response.authResponse.accessToken
-                    }, function(data, status) {       
-                        console.log("Data: " + data.key + "\nStatus: " + status);   
-                    });
-                    $("#play-btn").show();
-                    $("#login-btn").hide();
-                } else {
-                    $("#play-btn").hide();
-                    $("#login-btn").show();
-                    alert("Facebook Login Failed!");
-                }
-            });
+    });
+    FB.AppEvents.logPageView();
+};
+(function (d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) {
+        return;
     }
+    js = d.createElement(s);
+    js.id = id;
+    js.src = "https://connect.facebook.net/en_US/sdk.js";
+    fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+
+function checkLoginState() {
+    FB.getLoginStatus(function (response) {
+        if (response.status === 'connected') {
+            $.post("/rest-auth/facebook/", {
+                "access_token": response.authResponse.accessToken
+            }, function (data, status) {       
+                console.log("Data: " + data.key + "\nStatus: " + status);   
+            });
+            $("#play-btn").show();
+            $("#login-btn").hide();
+        } else {
+            $("#play-btn").hide();
+            $("#login-btn").show();
+            alert("Facebook Login Failed!");
+        }
+    });
+}
