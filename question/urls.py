87c37454e2158d8e09b5d34236760029e95aa5e7
Himanshu Chauhan <hchauhan1404@outlook.com>
2017-02-15 12:37:11 +0000

    Server Backup

--- question/urls.py
+++ question/urls.py
@@ -4,6 +4,8 @@ from question import views
 
 urlpatterns = [
     url(r'^$', views.getQuestion, name='question'),
+    url(r'^johncena/', views.getQuestion),
     url(r'^(?P<level>[0-9]+)/$', views.getQuestionByLevel, name='question_by_level'),
+    url(r'^(?P<level>[0-9]+)/johncena$', views.getQuestionByLevel),
     url(r'^answer/$', views.submitAnswer, name='answer'),
 ]
